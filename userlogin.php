330ec88662deecfcf6421bf77067fff79f0ff511
Allen <walkerallen@comcast.net>
2017-03-01 21:23:33 -0500

    updated html title in index.php, search.php and userlogin.php

--- userlogin.php
+++ userlogin.php
@@ -3,6 +3,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <title>Assignment 2</title>
   <meta charset='utf-8'>
   <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
   <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
